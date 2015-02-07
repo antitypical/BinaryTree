26c26519c9ed3b4c2aef18dd5cc443ab0bd9e5c9
Rob Rix <rob.rix@github.com>
2015-02-07 15:14:30 -0500

    Test that empty trees are not leaves.

--- BinaryTreeTests/BinaryTreeTests.swift
+++ BinaryTreeTests/BinaryTreeTests.swift
@@ -1,6 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-final class BinaryTreeTests: XCTestCase {}
+final class BinaryTreeTests: XCTestCase {
+	func testEmptyTreesAreNotLeaves() {
+		XCTAssertFalse(BinaryTree<Int>().isLeaf)
+	}
+}
 
 
+import BinaryTree
 import XCTest
