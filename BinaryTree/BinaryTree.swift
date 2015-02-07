a20413287c9349ff631c4be46e34d85e70939230
Rob Rix <rob.rix@github.com>
2015-02-07 14:32:01 -0500

    Docs.

--- BinaryTree/BinaryTree.swift
+++ BinaryTree/BinaryTree.swift
@@ -1,5 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
+/// A binary tree with leaves of type `T`.
 enum BinaryTree<T> {
 	case Leaf(Box<T>)
 	case Branch(Box<BinaryTree>, Box<BinaryTree>)
