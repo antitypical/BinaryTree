3a8407b28a9597feba6978cb9dd15954dc990748
Rob Rix <rob.rix@github.com>
2015-02-07 14:09:12 -0500

    Initial Commit

---
+++ BinaryTree/BinaryTree.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+/// Project version number for BinaryTree.
+extern double BinaryTreeVersionNumber;
+
+/// Project version string for BinaryTree.
+extern const unsigned char BinaryTreeVersionString[];
+
