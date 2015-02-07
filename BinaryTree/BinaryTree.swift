d62391476f25711c264870ecf445598b50936d82
Rob Rix <rob.rix@github.com>
2015-02-07 14:43:32 -0500

    Leaf constructor.

--- BinaryTree/BinaryTree.swift
+++ BinaryTree/BinaryTree.swift
@@ -9,6 +9,11 @@ public enum BinaryTree<T>: NilLiteralConvertible {
 		self = Nil
 	}
 
+	/// Constructs a leaf.
+	public init(_ value: T) {
+		self = Branch(Box(nil), Box(value), Box(nil))
+	}
+
 
 	// MARK: NilLiteralConvertible
 
